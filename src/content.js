253554c755a2a3819b0ac537f80e2879caa0a036
Jasper Kuperus <github@jasperkuperus.nl>
2019-08-12 16:08:02 +0200

    First successful XHR response capture

--- src/content.js
+++ src/content.js
@@ -1,27 +1,52 @@
-// Capture incomding HTTP data
-setTimeout(() => {
-  console.log('Loaded content.js');
-}, 2000);
+/**
+ * Contains the plugin code that will be injected in the page by
+ * the `injectPlugin` code.
+ */
+function plugin() {
+  /**
+   * Initializes the XHR interceptor. Overrides the XMLHttpRequest and
+   * correctly calls the handler.
+   */
+  (function initXHRInterceptor() {
+    var XHR = XMLHttpRequest.prototype;
+    var send = XHR.send;
+    var open = XHR.open;
 
-// Capture HTML changes
-// setTimeout(() => {
-//   const ul = $('div.stream ul.lazyLoadingList__list');
+    XHR.open = function(method, url) {
+      this.url = url; // the request url
+      return open.apply(this, arguments);
+    }
 
-//   function getItems() {
-//     return ul.find('li.soundList__item');
-//   }
+    XHR.send = function() {
+      this.addEventListener('load', function() {
+        if (this.url.includes('https://api-v2.soundcloud.com/stream')) {
+          xhrHandler(JSON.parse(this.response));
+        }
+      });
+      return send.apply(this, arguments);
+    };
+  })();
 
-//   const observer = new MutationObserver((mutations) => {
-//     // mutations.forEach((mutation) => {
-//     //   mutation.addedNodes.forEach((addedNode) => {
-//     //     insertedNodes.push(addedNode);
-//     //   })
-//     // })
+  /**
+   * Handles new incoming XHR data.
+   */
+  function xhrHandler(data) {
+    console.log('Handling XHR!', data);
+  }
+}
 
-//     console.log('Hello mutations:', mutations);
-//     console.log('Items', getItems());
-//   })
 
-//   observer.observe(ul.get(0), { childList: true });
-//   console.log('Items', getItems());
-// }, 2000);
+/**
+ * Injects the plugin code into the DOM. When `head` is ready,
+ * inject the `script` tag with the contents of our plugin function.
+ */
+(function injectPlugin() {
+  if (document.body && document.head) {
+    var xhrOverrideScript = document.createElement('script');
+    xhrOverrideScript.type = 'text/javascript';
+    xhrOverrideScript.innerHTML = `(${plugin.toString()})();`;
+    document.head.prepend(xhrOverrideScript);
+  } else {
+    requestIdleCallback(injectPlugin);
+  }
+})();
